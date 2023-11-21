d070c72930e42fcdfab0c03d05036cd1435eba6b
Arnav Gosain <arnav@arnavgosain.com>
2023-11-21 17:26:19 +0530

    Update

--- main.py
+++ main.py
@@ -7,25 +7,31 @@ pose = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5)
 
 def get_bounding_box(pose_landmarks, image_width, image_height):
     landmarks = pose_landmarks.landmark
-    x_min = min([landmark.x for landmark in landmarks if landmark.visibility > 0.5], default=0)
-    y_min = min([landmark.y for landmark in landmarks if landmark.visibility > 0.5], default=0)
-    x_max = max([landmark.x for landmark in landmarks if landmark.visibility > 0.5], default=1)
-    y_max = max([landmark.y for landmark in landmarks if landmark.visibility > 0.5], default=1)
+    # Consider only landmarks with a visibility higher than a threshold.
+    visible_landmarks = [landmark for landmark in landmarks if landmark.visibility > 0.5]
+    if not visible_landmarks:
+        # If no landmarks are visible, return an empty box.
+        return 0, 0, image_width, image_height
 
-    # Convert from relative to absolute coordinates.
-    x_min = int(x_min * image_width)
-    y_min = int(y_min * image_height)
-    x_max = int(x_max * image_width)
-    y_max = int(y_max * image_height)
+    x_min = min([landmark.x for landmark in visible_landmarks])
+    y_min = min([landmark.y for landmark in visible_landmarks])
+    x_max = max([landmark.x for landmark in visible_landmarks])
+    y_max = max([landmark.y for landmark in visible_landmarks])
 
-    # Add some padding.
-    padding = 10
-    x_min = max(0, x_min - padding)
-    y_min = max(0, y_min - padding)
-    x_max = min(image_width, x_max + padding)
-    y_max = min(image_height, y_max + padding)
+    # Convert from relative to absolute coordinates and add some padding.
+    padding = 10  # You might need to adjust this padding.
+    x_min = int(x_min * image_width) - padding
+    y_min = int(y_min * image_height) - padding
+    x_max = int(x_max * image_width) + padding
+    y_max = int(y_max * image_height) + padding
 
-    return (x_min, y_min, x_max - x_min, y_max - y_min)
+    # Clamp values to be within frame dimensions.
+    x_min = max(0, x_min)
+    y_min = max(0, y_min)
+    x_max = min(image_width, x_max)
+    y_max = min(image_height, y_max)
+
+    return x_min, y_min, x_max - x_min, y_max - y_min
 
 def detect_people(frame):
     image_height, image_width, _ = frame.shape
@@ -38,9 +44,26 @@ def detect_people(frame):
     
     return bounding_boxes
 
-def crop_frame(frame, box):
-    x, y, w, h = box
-    return frame[y:y+h, x:x+w]
+def resize_and_pad(crop, desired_width, desired_height):
+    # Calculate the new size to maintain aspect ratio.
+    h, w = crop.shape[:2]
+    scale = min(desired_width / w, desired_height / h)
+    new_w = int(w * scale)
+    new_h = int(h * scale)
+    resized_crop = cv2.resize(crop, (new_w, new_h))
+
+    # Create a new canvas with the desired size.
+    canvas = cv2.copyMakeBorder(
+        resized_crop,
+        top=(desired_height - new_h) // 2,
+        bottom=(desired_height - new_h) // 2,
+        left=(desired_width - new_w) // 2,
+        right=(desired_width - new_w) // 2,
+        borderType=cv2.BORDER_CONSTANT,
+        value=[0, 0, 0]  # Black padding.
+    )
+
+    return canvas
 
 def process_video(input_path, output_path):
     cap = cv2.VideoCapture(input_path)
@@ -65,26 +88,13 @@ def process_video(input_path, output_path):
         boxes = detect_people(frame)
         
         # Create crops based on the number of boxes.
-        if len(boxes) == 1:
-            # If there's only one person, crop the frame around them and resize to 720p.
-            crop = crop_frame(frame, boxes[0])
-            crop = cv2.resize(crop, (output_width, output_height))
-            out.write(crop)
-        elif len(boxes) == 2:
-            # If there are two people, create two crops and resize them to fit in 720p.
-            crop1 = crop_frame(frame, boxes[0])
-            crop2 = crop_frame(frame, boxes[1])
-            crop1 = cv2.resize(crop1, (output_width, int(output_height / 2)))
-            crop2 = cv2.resize(crop2, (output_width, int(output_height / 2)))
-            stacked_crop = cv2.vconcat([crop1, crop2])
-            out.write(stacked_crop)
-        else:
-            # Handle other cases, e.g., no people detected.
-            continue  # Skip the frame or handle as needed.
+        for box in boxes:
+            crop = frame[box[1]:box[1]+box[3], box[0]:box[0]+box[2]]
+            canvas = resize_and_pad(crop, output_width, output_height)
+            out.write(canvas)
     
     cap.release()
     out.release()
-
 
 # Call the processing function with the path to your input video and the desired output path.
 process_video('zuckerberg_trimmed.mp4', 'zuckerberg_trimmed-cropped.mp4')
